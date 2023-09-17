2d77cafa8cca7f2a284528c4626c65f43e23046d
Colin Haber <colin.haber@outlook.com>
2023-09-17 09:32:21 -0500

    Add project files.

---
+++ FaWindowManager.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Timers;
+using Timer = System.Timers.Timer;
+namespace ForgedAllianceFullscreen
+{
+	internal class FaWindowManager
+	{
+		private const nint HWND_TOP = 0;
+		private const uint SWP_SHOWWINDOW = 0x0040;
+		private const uint SWP_FRAMECHANGED = 0x0020;
+		private const int SW_SHOWMAXIMIZED = 3;
+		private const int SW_RESTORE = 9;
+		private const int GWL_STYLE = -16;
+		private const long WS_BORDERLESS = 0x16000000;
+		[DllImport("user32.dll")]
+		private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int x, int y, int width, int height, uint uFlags);
+		[DllImport("user32.dll")]
+		private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+		[DllImport("user32.dll")]
+		private static extern long GetWindowLongW(IntPtr hWnd, int nIndex);
+		[DllImport("user32.dll")]
+		private static extern long SetWindowLongW(IntPtr hWnd, int nIndex, long dwNewLong);
+		private readonly ISet<int> fullscreenedProcessIds;
+		private readonly int x;
+		private readonly int y;
+		private readonly int width;
+		private readonly int height;
+		private readonly ILogger logger;
+		internal FaWindowManager(int x, int y, int width, int height)
+		{
+			using var loggerFactory = LoggerFactory.Create(builder =>
+			{
+				builder.AddFilter("Microsoft", LogLevel.Warning)
+							 .AddFilter("System", LogLevel.Warning)
+							 .AddFilter("LoggingConsoleApp.Program", LogLevel.Debug)
+							 .AddSimpleConsole(options =>
+							 {
+								 options.SingleLine = true;
+							 });
+			});
+			logger = loggerFactory.CreateLogger<FaWindowManager>();
+			fullscreenedProcessIds = new HashSet<int>();
+			this.x = x;
+			this.y = y;
+			this.width = width;
+			this.height = height;
+		}
+		internal void Run()
+		{
+			CheckFaProcessWindows();
+		}
+		internal Task Watch()
+		{
+			var timer = new Timer(TimeSpan.FromMilliseconds(100));
+			timer.Elapsed += (object? sender, ElapsedEventArgs e) =>
+			{
+				timer.Stop();
+				CheckFaProcessWindows();
+				timer.Start();
+			};
+			timer.Start();
+			logger.LogInformation(@"Watching for Forged Alliance processes...");
+			return Task.Delay(-1);
+		}
+		private void CheckFaProcessWindows()
+		{
+			var faProcesses = Process.GetProcessesByName(@"ForgedAlliance");
+			foreach (var faProcess in faProcesses)
+			{
+				var pid = faProcess.Id;
+				if (fullscreenedProcessIds.Add(pid))
+				{
+					logger.LogInformation(@"[{ProcessId}] Detected {ProcessName} process", pid, faProcess.ProcessName);
+					SpinWait.SpinUntil(() => faProcess.MainWindowHandle != 0); // Wait for the main window to exist before trying to manipulate it
+					logger.LogInformation(@"[{ProcessId}] Detected main window", pid);
+					SetBorderlessAndResize(pid, faProcess.MainWindowHandle);
+					logger.LogInformation(@"[{ProcessId}] Window ready", pid);
+					faProcess.Exited += (object? sender, EventArgs e) =>
+					{
+						fullscreenedProcessIds.Remove(pid);
+						logger.LogInformation(@"[{ProcessId}] Process exited, freeing PID", pid);
+					};
+					faProcess.EnableRaisingEvents = true;
+				}
+			}
+		}
+		private void SetBorderlessAndResize(int pid, nint windowHandle)
+		{
+			ShowWindow(windowHandle, SW_RESTORE); // Can't resize window if it's minimized/maximized
+			SetWindowLongW(windowHandle, GWL_STYLE, WS_BORDERLESS); // Remove frame from window style
+			logger.LogInformation(@"[{ProcessId}] Made window borderless", pid);
+			SetWindowPos(windowHandle, HWND_TOP, x, y, width, height, SWP_SHOWWINDOW | SWP_FRAMECHANGED); // Resize and apply window style
+			logger.LogInformation(@"[{ProcessId}] Resized window", pid);
+			ShowWindow(windowHandle, SW_SHOWMAXIMIZED); // Maximize window (seems to help drawing work correctly)
+			ShowWindow(windowHandle, SW_RESTORE); // Restore to desired size after maximizing
+			logger.LogInformation(@"[{ProcessId}] Redrew contents", pid);
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.CommandLine;
+namespace ForgedAllianceFullscreen
+{
+	internal class Program
+	{
+		static async Task<int> Main(string[] args)
+		{
+			var rootCommand = new RootCommand(@"Run Forged Alliance in borderless windowed mode");
+			var watchCommand = new Command(@"--watch", description: @"Run in the background, updating new Forged Alliance windows as they are created");
+			var xOption = new Option<int>(@"--x", () => 0, description: @"The position of the left edge of the window relative to the left edge of the primary display in pixels");
+			var yOption = new Option<int>(@"--y", () => 0, description: @"The position of the top of the window relative to the top of the primary display in pixels");
+			var widthOption = new Option<int>(@"--width", description: @"The desired window width in pixels");
+			var heightOption = new Option<int>(@"--height", description: @"The desired window height in pixels");
+			rootCommand.AddCommand(watchCommand);
+			rootCommand.AddGlobalOption(xOption);
+			rootCommand.AddGlobalOption(yOption);
+			rootCommand.AddGlobalOption(widthOption);
+			rootCommand.AddGlobalOption(heightOption);
+			rootCommand.SetHandler((x, y, width, height) =>
+			{
+				new FaWindowManager(x, y, width, height).Run();
+			}, xOption, yOption, widthOption, heightOption);
+			watchCommand.SetHandler((x, y, width, height) =>
+			{
+				new FaWindowManager(x, y, width, height).Watch().Wait();
+			}, xOption, yOption, widthOption, heightOption);
+			
+			return await rootCommand.InvokeAsync(args);
+		}
+	}
+}
