4756396d1cd001acde1f58c4782f45aab987a7c9
Colin Haber <colin.haber@outlook.com>
2023-09-18 06:50:27 -0500

    Make --watch an option rather than a command

--- ForgedAllianceFullscreen.Shared/Program.cs
+++ ForgedAllianceFullscreen.Shared/Program.cs
@@ -6,27 +6,28 @@ namespace ForgedAllianceFullscreen
 		static async Task<int> Main(string[] args)
 		{
 			var rootCommand = new RootCommand(@"Run Forged Alliance in borderless windowed mode");
-			var watchCommand = new Command(@"--watch", description: @"Run in the background, updating new Forged Alliance windows as they are created");
+			var watchOption = new Option<bool>(@"--watch", description: @"Run in the background, updating new Forged Alliance windows as they are created");
 			var xOption = new Option<int>(@"--x", () => 0, description: @"The position of the left edge of the window relative to the left edge of the primary display in pixels");
 			var yOption = new Option<int>(@"--y", () => 0, description: @"The position of the top of the window relative to the top of the primary display in pixels");
 			var widthOption = new Option<int>(@"--width", description: @"The desired window width in pixels");
 			var heightOption = new Option<int>(@"--height", description: @"The desired window height in pixels");
 			var cpuOption = new Option<bool>(@"--cpu", description: @"Optimize CPU usage by setting core affinity and process priority");
-			rootCommand.AddCommand(watchCommand);
+			rootCommand.AddGlobalOption(watchOption);
 			rootCommand.AddGlobalOption(xOption);
 			rootCommand.AddGlobalOption(yOption);
 			rootCommand.AddGlobalOption(widthOption);
 			rootCommand.AddGlobalOption(heightOption);
 			rootCommand.AddGlobalOption(cpuOption);
-			rootCommand.SetHandler((x, y, width, height, cpu) =>
+			rootCommand.SetHandler((watch, x, y, width, height, cpu) =>
 			{
-				new FaWindowManager(x, y, width, height, cpu).Run();
-			}, xOption, yOption, widthOption, heightOption, cpuOption);
-			watchCommand.SetHandler((x, y, width, height, cpu) =>
-			{
-				new FaWindowManager(x, y, width, height, cpu).Watch().Wait();
-			}, xOption, yOption, widthOption, heightOption, cpuOption);
-			
+				if (watch)
+				{
+					new FaWindowManager(x, y, width, height, cpu).Watch().Wait();
+				} else
+				{
+					new FaWindowManager(x, y, width, height, cpu).Run();
+				}
+			}, watchOption, xOption, yOption, widthOption, heightOption, cpuOption);
 			return await rootCommand.InvokeAsync(args);
 		}
 	}
